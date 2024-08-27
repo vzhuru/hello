e0935026e61e25ae95a617ff3735339f464ddd1b
vzhuru <scholbergg@gmail.com>
2024-08-27 12:47:19 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Garrett Scholberg
+ * Date: 2024/08/24
+ *
+ * A simple hello world program in C
+ *
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello Rhett!\n");
+
+    return 0;
+}
